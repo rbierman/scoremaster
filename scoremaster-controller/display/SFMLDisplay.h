63b01e8b7bdaf7841cf6f32fbc11cc782536e131
Ron Bierman <bierman.ron@gmail.com>
2026-02-18 13:40:53 -0800

    Cleanup IDE files and unused includes

--- scoremaster-controller/display/SFMLDisplay.h
+++ scoremaster-controller/display/SFMLDisplay.h
@@ -2,15 +2,11 @@
 
 #include "IDisplay.h"
 #include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Graphics/RectangleShape.hpp> // New include
-#include <SFML/Graphics/Color.hpp> // New include
 
 class DoubleFramebuffer;
 
 class SFMLDisplay : public IDisplay {
     sf::RenderWindow window;
-    // Removed sf::Texture texture;
-    // Removed sf::Sprite sprite;
 
     const unsigned int PIXEL_SIZE = 4; // Size of each simulated pixel
     const unsigned int PIXEL_GAP = 1;  // Gap between simulated pixels
